76d254ab148ff2712f08f1f0838fe6de763f5e42
hollysmason <ibondar204@gmail.com>
2016-02-18 00:46:59 +0200

    Without array.

--- script.js
+++ script.js
@@ -4,26 +4,23 @@ $(document).ready(function () {
     $("input[name='email']").val('').focus();
   });
   $("button.submit").click(function () {
-    var users = [];
     var User = function (name, email) {
       this.name = name;
       this.email = email;
     }
-    users.push(new User(
+    var user = new User(
       $("input[name='username']").val(),
       $("input[name='email']").val()
-    ));
-    users.forEach(function(user){
-      $("tbody").append(
-        '<tr>' +
-          '<td class="name">' + user.name + '</td>' +
-          '<td class="mail">' + user.email + '</td>' +
-          '<td>' +
-            '<button type="button"  class="btn btn-default">Edit</button>' +
-            '<button type="button" id="buttonDelete" class="btn btn-default">Delete</button>' +
-          '</td>' +
-        '</tr>'
-      );
-    });
+    );
+    $("tbody").append(
+      '<tr>' +
+        '<td class="name">' + user.name + '</td>' +
+        '<td class="mail">' + user.email + '</td>' +
+        '<td>' +
+          '<button type="button"  class="btn btn-default">Edit</button>' +
+          '<button type="button" id="buttonDelete" class="btn btn-default">Delete</button>' +
+        '</td>' +
+      '</tr>'
+    );
   });
 });
