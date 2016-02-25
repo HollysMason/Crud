d0e594a3889495f6556e1ff3e3caf871e749711f
hollysmason <ibondar204@gmail.com>
2016-02-26 00:02:28 +0200

    Added delete handler and working on edit handler.

--- script.js
+++ script.js
@@ -12,15 +12,41 @@ $(document).ready(function () {
       $("input[name='username']").val(),
       $("input[name='email']").val()
     );
-    $("tbody").append(
+    $("tbody.maindata").append(
       '<tr>' +
         '<td class="name">' + user.name + '</td>' +
         '<td class="mail">' + user.email + '</td>' +
         '<td>' +
-          '<button type="button"  class="btn btn-default">Edit</button>' +
-          '<button type="button" id="buttonDelete" class="btn btn-default">Delete</button>' +
+          '<button type="button" class="btn btn-default" onclick="editUser()">Edit</button>' +
+          '<button type="button"  class="btn btn-default" onclick="deleteUser()">Delete</button>' +
         '</td>' +
       '</tr>'
     );
   });
 });
+
+var editUser = function () {
+  var EditUser = function (editName, editEmail) {
+    this.editName = editName;
+    this.editEmail = editEmail;
+  }
+
+  var editUser = new EditUser(changedName, changedEmail);
+  var cur = $(event.target).parent().parent().replaceWith(
+      '<tr>' +
+        '<td class="name">' + editUser.editName + '</td>' +
+        '<td class="name">' + editUser.editEmail + '</td>' +
+      '<td>' +
+        '<button type="button" class="btn btn-default" onclick="edit()">Edit</button>' +
+        '<button type="button"  class="btn btn-default" onclick="deleteUser()">Delete</button>' +
+      '</td>' +
+    '</tr>'
+  );
+}
+
+var deleteUser = function () {
+  var confirmWindow = confirm("Are you sure ?");
+  if (confirmWindow == true) {
+      var current = $(event.target).parent().parent().remove();
+  }
+}
